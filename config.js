0332d9fe525974ab68992b8775dec16fcc6f8903
Felipe Rodrigues <160525826+feliperodrigues7@users.noreply.github.com>
2025-10-07 21:15:33 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js — cole aqui SEUS VALORES reais uma única vez
+export const SUPABASE_URL = 'https://sfffjmtklryxjwmvsqfa.supabase.co';
+export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmZmZqbXRrbHJ5eGp3bXZzcWZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4MDMzNDcsImV4cCI6MjA3NTM3OTM0N30.IP4DVQqeN37xJ4QyGBUQJDT7xSxpTfpcDzbG7ZDbAnI';
